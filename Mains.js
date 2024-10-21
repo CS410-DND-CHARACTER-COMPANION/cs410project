6cb889e066b4ca6578702d0c47dc067df0d1d344
UserDOTJun <112987719+UserDOTJun@users.noreply.github.com>
2024-10-21 04:20:41 -0400

    upd1

---
+++ Mains.js
@@ -0,0 +1,14 @@
+console.log("Hi")
+
+// const BaseURL = "http://localhost:3000/"
+
+// const TextBoxA = document.getElementById("TextBoxA")
+
+// async function ReceivedUserInput(event)
+// {
+//     event.preventDefault() // Returns false if default action occurs(?)
+    
+//     const UserInput = await fetch(BaseURL, {method: "TextBoxA"})
+//     console.log(UserInput)
+// }
+// TextBoxA.addEventListener("change", ReceivedUserInput)
---
+++ app.js
@@ -0,0 +1,21 @@
+// var msg = 'Hello World';
+// console.log(msg);
+
+// Server Set Up
+const express = require("express")
+const path = require("path")
+const app = express()
+// Port things
+const port = 3000
+app.listen(port, () => {
+  console.log("DnD app listening on port " + port)
+})
+// Server Set Up (End)
+
+// The "main" function when User gets in the address
+app.get('/', (req, res) => {
+  // Connect to frontend file
+  res.sendFile(path.join(__dirname, 'frontend', 'index.html'))
+})
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
