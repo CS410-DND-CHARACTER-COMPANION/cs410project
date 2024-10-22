b7e04bdcd92630393e872544e4193509c8b8f17e
niconicode <nicolestrounine@gmail.com>
2024-10-22 15:49:06 -0400

    Edit database connection

--- db.js
+++ db.js
@@ -1,31 +1,29 @@
-// Database
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://<GroupUser>:<O34qQAVRCa0g89hP>@cluster0.gjnf5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const {MongoClient} = require('mongodb');
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+async function main(){
+  /**
+   * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+   * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+   */
+  const uri = "mongodb+srv://GroupUser:cs410project@cluster0.gjnf5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-async function run() {
+  const client = new MongoClient(uri);
+
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    // list databases
-    await  listDatabases(client);
+      // Connect to the MongoDB cluster
+      await client.connect();
+
+      // Make the appropriate DB calls
+      await  listDatabases(client);
+
+  } catch (e) {
+      console.error(e);
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+      await client.close();
   }
 }
-run().catch(console.dir);
+
+main().catch(console.error);
 
 async function listDatabases(client){
   databasesList = await client.db().admin().listDatabases();
