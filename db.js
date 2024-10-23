ae9c9af79f84154169f4cfe04f97f892870fd34b
niconicode <nicolestrounine@gmail.com>
2024-10-23 10:22:21 -0400

    Insert character into database

--- db.js
+++ db.js
@@ -1,5 +1,4 @@
-// Currently when you run "node db.js" it simply connects to the database
-// and prints a list of the databases in the cluster.
+// connects to database and creates a character and inserts it into the collection
 
 const {MongoClient} = require('mongodb');
 
@@ -16,8 +15,15 @@ async function main(){
       // Connect to the MongoDB cluster
       await client.connect();
 
-      // Make the appropriate DB calls
-      await  listDatabases(client);
+      // Create a character and add it to the database
+      await createCharacter(client, {
+        name: "Gorgug Thistlespring",
+        race: "Half-orc",
+        class: "Barbarian",
+        level: 8,
+        AC: 14,
+        HP: 112
+      });
 
   } catch (e) {
       console.error(e);
@@ -27,6 +33,13 @@ async function main(){
 }
 
 main().catch(console.error);
+
+async function createCharacter(client, newCharacter) {
+  // New character inserted into collection
+  const result = await client.db("dnd_screen").collection("character_sheets").insertOne(newCharacter);
+
+  console.log(`New character created with the following id: ${result.insertedId}`);
+}
 
 async function listDatabases(client){
   databasesList = await client.db().admin().listDatabases();
