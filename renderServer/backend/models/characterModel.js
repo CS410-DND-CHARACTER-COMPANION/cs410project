55939af0da4c39b22ec9090dc853fe05a840bf25
BBuns <168569587+baodiep1@users.noreply.github.com>
2024-11-20 15:17:48 -0500

    Update characterModel.js

--- renderServer/backend/models/characterModel.js
+++ renderServer/backend/models/characterModel.js
@@ -1,12 +1,17 @@
-// backend/models/characterModel.js
 const mongoose = require('mongoose');
 
 const characterSchema = new mongoose.Schema({
     name: String,
+    species: String,
     class: String,
     level: Number,
+    ac: Number,
+    currentHP: Number,
+    maxHP: Number,
+    inventory: Array,
     // Add other fields as needed
 });
 
 const Character = mongoose.model('Character', characterSchema);
+
 module.exports = Character;
