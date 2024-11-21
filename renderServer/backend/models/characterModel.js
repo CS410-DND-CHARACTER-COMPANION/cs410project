9e40e21aecd16116d6c5f1e8efff5c22fa44cd13
BBuns <168569587+baodiep1@users.noreply.github.com>
2024-11-20 21:05:44 -0500

    Update characterModel.js

--- renderServer/backend/models/characterModel.js
+++ renderServer/backend/models/characterModel.js
@@ -1,17 +1,20 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'); // Import the mongoose library for MongoDB object modeling
 
+// Define the character schema with relevant fields
 const characterSchema = new mongoose.Schema({
-    name: String,
-    species: String,
-    class: String,
-    level: Number,
-    ac: Number,
-    currentHP: Number,
-    maxHP: Number,
-    inventory: Array,
+    name: String,         // Character's name
+    species: String,      // Character's species
+    class: String,        // Character's class
+    level: Number,        // Character's level
+    ac: Number,           // Armor Class
+    currentHP: Number,    // Current Hit Points
+    maxHP: Number,        // Maximum Hit Points
+    inventory: Array,     // Inventory items (array)
     // Add other fields as needed
 });
 
+// Create a model for the Character using the defined schema
 const Character = mongoose.model('Character', characterSchema);
 
+// Export the Character model for use in other parts of the application
 module.exports = Character;
